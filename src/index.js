62bc8e5b3b27d3b02ab6e1cb368fb3705b215fb5
Kanami <kanami.h217@gmail.com>
2019-10-25 21:18:27 +0900

    add currentTime, fixed elements

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment-timezone';
 import './index.css';
 import App from './App';
 import AppTwo from './AppTwo';
-import AppWithProps from './AppWithProps';
 import * as serviceWorker from './serviceWorker';
 
-const elements = (
-    <div>
-        <App/><AppWithProps/>
-        <AppTwo/>
-    </div>
-)
+const currentTime = moment.tz(moment(), 'Asia/Tokyo').format('YYYY年MM月DD日');
 
-ReactDOM.render(
-    elements,
-    document.getElementById('root')
-);
+// App.jsの受け渡し
+const firstH1 = React.createElement(App, { currentTime }, null);
+ReactDOM.render(firstH1, document.getElementById('root'));
+
+//AppTwo.jsの受け渡し
+const secondH1 = React.createElement(AppTwo, null, null);
+ReactDOM.render(secondH1, document.getElementById('second-root'));
+
+// const elements = (
+//     <div>
+//         <App/><AppWithProps/>
+//         <AppTwo/>
+//     </div>
+// )
+
+// ReactDOM.render(
+//     elements,
+//     document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
